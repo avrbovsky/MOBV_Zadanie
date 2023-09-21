9743d70c74f9fea5711fd2c3db3eae0b4cb2c3ea
Adrián Vrbovský <avrbovsky64@gmail.com>
2023-09-21 08:41:52 +0200

    Initial commit

---
+++ app/src/main/java/eu/mcomputing/mobv/zadanie/MainActivity.kt
@@ -0,0 +1,14 @@
+package eu.mcomputing.mobv.zadanie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("Mojtag","moja info")
+
+    }
+}
