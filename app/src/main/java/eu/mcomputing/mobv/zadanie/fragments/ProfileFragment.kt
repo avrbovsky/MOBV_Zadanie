54f47936519f965658eaf51e9905a54a01519bb2
Adrián Vrbovský <avrbovsky64@gmail.com>
2023-10-29 22:32:12 +0100

    5.4: OAuth autorization

--- app/src/main/java/eu/mcomputing/mobv/zadanie/fragments/ProfileFragment.kt
+++ app/src/main/java/eu/mcomputing/mobv/zadanie/fragments/ProfileFragment.kt
@@ -3,11 +3,29 @@ package eu.mcomputing.mobv.zadanie.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
 import eu.mcomputing.mobv.zadanie.R
+import eu.mcomputing.mobv.zadanie.data.PreferenceData
+import eu.mcomputing.mobv.zadanie.data.api.DataRepository
 import eu.mcomputing.mobv.zadanie.databinding.FragmentProfileBinding
+import eu.mcomputing.mobv.zadanie.viewmodels.ProfileViewModel
 
 class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private lateinit var viewModel: ProfileViewModel
     private var binding: FragmentProfileBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity(), object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return ProfileViewModel(DataRepository.getInstance()) as T
+            }
+        })[ProfileViewModel::class.java]
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,7 +33,33 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         binding = FragmentProfileBinding.bind(view).apply {
             lifecycleOwner = viewLifecycleOwner
         }.also { bnd ->
+            bnd.btLoadProfile.setOnClickListener {
+                val user = PreferenceData.getInstance().getUser(requireContext())
+                user?.let {
+                    viewModel.loadUser(it.id, it.id, it.access, it.refresh)
+                }
+            }
 
+            bnd.btLogout.setOnClickListener {
+                PreferenceData.getInstance().clearData(requireContext())
+                it.findNavController().navigate(R.id.action_profile_to_intro)
+            }
+
+            viewModel.profileResult.observe(viewLifecycleOwner) {
+                if (it.isNotEmpty()) {
+                    Snackbar.make(
+                        bnd.btLoadProfile,
+                        it,
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
+        viewModel.userResult.observe(viewLifecycleOwner) {
+            it?.let { profile ->
+                PreferenceData.getInstance().putUser(requireContext(), profile)
+            }
         }
     }
 
